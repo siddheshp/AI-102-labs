f14205c91507fb37019b7205ac79cc223ed4d76a
Siddhesh Prabhugaonkar <siddheshpg@gmail.com>
2024-05-30 13:13:10 +0530

    initial

---
+++ mslearn-ai-language-main/Labfiles/06b-translator-sdk/C-Sharp/translate-text/Program.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+// import namespaces
+using Azure;
+using Azure.AI.Translation.Text;
+
+
+namespace translate_text
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Set console encoding to unicode
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string translatorRegion = configuration["TranslatorRegion"];
+                string translatorKey = configuration["TranslatorKey"];
+
+
+                // Create client using endpoint and key
+                AzureKeyCredential credential = new(translatorKey);
+                TextTranslationClient client = new(credential, translatorRegion);
+
+                // Choose target language
+                Response<GetLanguagesResult> languagesResponse = await client.GetLanguagesAsync(scope: "translation").ConfigureAwait(false);
+                GetLanguagesResult languages = languagesResponse.Value;
+                Console.WriteLine($"{languages.Translation.Count} languages available.\n(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)");
+                Console.WriteLine("Enter a target language code for translation (for example, 'en'):");
+                string targetLanguage = "xx";
+                bool languageSupported = false;
+                while (!languageSupported)
+                {
+                    targetLanguage = Console.ReadLine();
+                    if (languages.Translation.ContainsKey(targetLanguage))
+                    {
+                        languageSupported = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{targetLanguage} is not a supported language.");
+                    }
+
+                }
+
+
+                // Translate text
+                string inputText = "";
+                while (inputText.ToLower() != "quit")
+                {
+                    Console.WriteLine("Enter text to translate ('quit' to exit)");
+                    inputText = Console.ReadLine();
+                    if (inputText.ToLower() != "quit")
+                    {
+                        Response<IReadOnlyList<TranslatedTextItem>> translationResponse = await client.TranslateAsync(targetLanguage, inputText).ConfigureAwait(false);
+                        IReadOnlyList<TranslatedTextItem> translations = translationResponse.Value;
+                        TranslatedTextItem translation = translations[0];
+                        string sourceLanguage = translation?.DetectedLanguage?.Language;
+                        Console.WriteLine($"'{inputText}' translated from {sourceLanguage} to {translation?.Translations[0].To} as '{translation?.Translations?[0]?.Text}'.");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ mslearn-ai-language-main/Labfiles/07-speech/C-Sharp/speaking-clock/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+// Import namespaces
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace speaking_clock
+{
+    class Program
+    {
+        private static SpeechConfig speechConfig;
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string aiSvcKey = configuration["SpeechKey"];
+                string aiSvcRegion = configuration["SpeechRegion"];
+
+                // Configure speech service
+                speechConfig = SpeechConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+                Console.WriteLine("Ready to use speech service in " + speechConfig.Region);
+
+                // Configure voice
+                speechConfig.SpeechSynthesisVoiceName = "en-US-AriaNeural";
+
+                // Get spoken input
+                string command = "";
+                command = await TranscribeCommand();
+                if (command.ToLower() == "what time is it?")
+                {
+                    await TellTime();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task<string> TranscribeCommand()
+        {
+            string command = "";
+
+            // Configure speech recognition
+            using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            Console.WriteLine("Speak now...");
+
+            // Configure speech recognition (audio input from file)
+            // string audioFile = "time.wav";
+            // SoundPlayer wavPlayer = new SoundPlayer(audioFile);
+            // wavPlayer.Play();
+            // using AudioConfig audioConfig = AudioConfig.FromWavFileInput(audioFile);
+            // using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            // Process speech input
+            SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+            if (speech.Reason == ResultReason.RecognizedSpeech)
+            {
+                command = speech.Text;
+                Console.WriteLine(command);
+            }
+            else
+            {
+                Console.WriteLine(speech.Reason);
+                if (speech.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(speech);
+                    Console.WriteLine(cancellation.Reason);
+                    Console.WriteLine(cancellation.ErrorDetails);
+                }
+            }
+
+            // Return the command
+            return command;
+        }
+
+        static async Task TellTime()
+        {
+            var now = DateTime.Now;
+            string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+
+            // Configure speech synthesis
+            speechConfig.SpeechSynthesisVoiceName = "en-GB-RyanNeural"; //en-GB-LibbyNeural
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+
+            // Synthesize spoken output
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
+
+            // Print the response
+            Console.WriteLine(responseText);
+        }
+
+    }
+}
